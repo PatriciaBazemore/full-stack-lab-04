9f67c28d73f295834ca93cb1f1a0b820825b5da0
PatriciaBazemore <trishbazemore@gmail.com>
2018-08-28 11:12:56 -0400

    clean up some redundancies

--- lab04_starter/script.js
+++ lab04_starter/script.js
@@ -4,130 +4,61 @@ var rainbow = ['blue', 'purple', 'red', 'yellow', 'green', 'orange']
 
 document.addEventListener('DOMContentLoaded', function() {
         var btn = document.createElement("button");
-        btn.innerText="Add Square";
+        btn.innerText= "Add Square";
         btn.addEventListener('click', createBox);
         document.body.appendChild(btn);
 
-
-    // var container = document.createElement('div');
-    //     container.id = 'square container';
-    //     document.body.appendChild(container);
-
+    var container = document.createElement('div');
+        container.id = 'square container';
+        document.body.appendChild(container);
+    
     function createBox(){
-        var div = document.createElement(div);
+        var boxNumber = document.getElementsByClassName('square').length;
+        var sq = document.createElement('div');
         var square = document.getElementsByClassName("Square");
-        var boxNumber = square.length;
-        // var existingSquares = document.getElementsByClassName('square').length;
-        div.className = "Square"; 
-        // container.appendChild(sq);   
-        div.id= 'boxNumber';
-        div.addEventListenr('click', color);
-        div.innerText = boxNumber;
-        div.addEventListener('mouseenter', hoover);
-        div.addEventListener('dblclick', disappear);
-
+        sq.className = "square"; 
+        container.appendChild(sq);   
+        sq.id = boxNumber;
+        sq.addEventListener('click', color);
+        sq.addEventListener('mouseenter', insertId);
+        sq.addEventListener('mouseleave', removeId);
+        sq.addEventListener('dblclick', disappear);
 }
 
-// function addSquare(){
-//     var sq = document.createElement(div);
-//     var existingSquares = document.getElementsByClassName('square').length;
-//     var square = document.getElementsByClassName("Square");
-//     sq.className = "square"; 
-//     container.appendChild(sq);
-//     sq.id = existingSquares;
-//     sq.addEventListener('click', changeColor);
-//     sq.addEventListener('mouseenter', insertId);
-//     sq.addEventListener('mouseleave', removeId);
-//     sq.addEventListener('dblclick', removeNeighbor);
-//  }
+    function insertId() {
+        var id = this.id;
+        this.innerText = id;
+    }
 
-        
+    function removeId() {
+        this.innerText = '';
+    }
 
-function hoover (){
-    mouseenter = div.innerText(id);
-    mouseleave = div.disappear(id);
-}
-
-// function insertId() {
-//     var id = this.id;
-//     this.innerText = id;
-
-// }
-
-// function removeId() {
-//     this.innerText = '';
-// }
-
-function disappear (){
-    if (boxNumber % 2 === 0){
-        remove(this++1) {
-            if 
+    function disappear() {
+        var id = this.id;
+        if (id % 2 === 0) {
+            if (this.nextSibling) {
+                this.nextSibling.remove(); 
+            } else {
+                alert('There are no squares to the right to remove!');
+            }
+        } else {
+            if (this.previousSibling) {
+                this.previousSibling.remove();
+            } else {
+                alert('There are no squares to the left to remove!');
+            }
         }
+    }
 
-    } else {
-        remove (this--1){
-
-        }
-     }
-}
-//       for (var i = 0; i < square.length; i++){
-          
-//       text"No More Squares!"       
-              
-//                var box = Square[0];
-//              box.innerText=" box ";
-// }
-
-// function removeNeighbor() {
+    function color(event){
+        var index = Math.floor(Math.random() * rainbow.length);
+        var rainbowIndv = rainbow[index];
+        //we need to set newColor as the background of the clicked div
+        this.style.backgroundColor = rainbowIndv
+        //event.target.style.backgroundColor = newColor;
+    }
     
-    
-//     var id = this.id;
-
-//     if (id % 2 === 0) {
-//         //if even, remove the square after
-//         if (this.nextSibling) {
-//             this.nextSibling.remove();
-//         } else {
-//             alert('There are no elements to the right to remove.');
-//         }
-//     } else {
-//         //if odd, remove the sqaure after
-//         if (this.previousSibling) {
-//             this.previousSibling.remove();
-//         } else {
-//             alert('There are no elements to the left to remove');
-//         }
-//     }
-
-// }
-
-
-function color(){
-    background-color= rainbow[0]
-}
-
-var rainbowIndv= rainbow[0]
-
-rainbowIndv= (i = 0 ; i > rainbow.length; i++){
-    background-color: rainbow[i];
-
-} 
-
-div id=clicks
-var clicks= number of times btn was clicked before 
-        
-btn.addEventListner('click', function(){
-
-})
-
-// function changeColor(event){  
-//     var index = Math.floor(Math.random() * colors.length) 
-//     var newColor = colors[index];
-//     //we need to set newColor as the background of the clicked div
-//     this.style.backgroundColor = newColor;
-//     //event.target.style.backgroundColor = newColor;
-// }
-
-
+});
 
 
